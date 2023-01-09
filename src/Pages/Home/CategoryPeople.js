317fabd839a32ecb8f5dc3552e5811a876d0f12e
Tsering Dorjee Lama <villadorje29@gmail.com>
2023-01-09 19:45:25 +0545

    Category people is completed

--- src/Pages/Home/CategoryPeople.js
+++ src/Pages/Home/CategoryPeople.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from '../../Components/ui/Button'
 import CategoryPeopleData from '../../Data/CategoryPeopleData'
 
 export default function CategoryPeople() {
@@ -14,6 +15,21 @@ export default function CategoryPeople() {
                     </div>
                   ))}
                   </div>
+        </div>
+        <div className="container-fluid bg-light">
+          <div className="container p-5"> 
+            <div className="row g-5 align-items-center">
+              <div className="phone col-lg-6 col-12">
+                <img src={require("../../Assets/img/mobile.png")} alt="" className='w-100' />
+               </div>
+               <div className='col-lg-6 col-12'>
+                <p className='text-secondary lh-1'>SEARCH PLACES</p>
+                <h2 className='fs-1 fw-normal lh-base'>People's Experiences</h2>
+                <p className='text-secondary lh-lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus est pellen tesque elit ullamcorper dignissim cras tincidunt lobortis feugiat. Et netus malesuada fames ac turpis egestas. Nisl vel pretium.</p>
+                <Button />
+               </div>
+            </div>
+          </div>
         </div>
     </>
   )
