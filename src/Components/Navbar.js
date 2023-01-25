f0e451e2e8ce6282688ee01eb05c4479182f2678
Tsering Dorjee Lama <villadorje29@gmail.com>
2023-01-25 15:42:54 +0545

    responsive complete

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export default function Navbar({size}) {
   return (
     <>
-      <div className="Navbar-container container-fluid bg-black p-4 position-sticky top-0">
+      <div className="Navbar-container container-fluid bg-black p-4 sticky-top">
         <div className="Navbar-flex row navbar-bar text-white align-items-center">
           <div className="navbar-img col-lg-2 text-lg-start text-center">
             <Link to={"/"} className=""><img src={require("../Assets/img/Logo.png")} alt="Logo" className='footer-logo ' /></Link>
@@ -124,62 +124,62 @@ export default function Navbar({size}) {
         </div>
       </div>
       <div className="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabIndex="-1">
-                <div className="modal-dialog modal-dialog-centered ">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <button className="btn w-50 p-2 bg-success " ata-bs-target="#exampleModalToggle" data-bs-toggle="modal">Login</button>
-                            <button className="btn w-50 bg-dark text-white p-2" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Register</button>
-                        </div>
-                        <div className="modal-body px-4">
-                            <input type="email" placeHolder='Email*' className='w-100 p-2' />
-                            <input type="password" placeHolder='Password*' className='w-100 p-2 mt-4' />
-                            <div className="dflex justify-content-between">
-                                <div className="input-group my-3 d-flex justify-content-between align-items-center">
-                                    <div className="input-group-text ">
-                                        <input className="form-check-input mt-0 me-2 " type="checkbox" value="" aria-label="Checkbox for following text input" />
-                                        <p className='mt-3'>Remember me?</p>
-                                    </div>
-                                    <p className='mt-2'>Forgot Password?</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="modal-footer p-3 text-center">
-                            <button className='btn bg-success text-white  w-100 p-2' >LOGIN</button>
-                            <p className='w-100'>or Connect with Social Networks</p>
-                            <div className='w-100'><i className="bi bi-facebook"></i></div>
-                            <p className='w-100'>By creating an account you are accepting our Terms & Conditions</p>
-                        </div>
-                    </div>
-                </div>
+        <div className="modal-dialog modal-dialog-centered ">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button className="btn w-50 p-2 bg-success " ata-bs-target="#exampleModalToggle" data-bs-toggle="modal">Login</button>
+              <button className="btn w-50 bg-dark text-white p-2" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Register</button>
             </div>
-            <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabIndex="-1">
-                <div className="modal-dialog modal-dialog-centered">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <button className="btn text-white bg-dark w-50 p-2" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Login</button>
-                            <button className="btn btn-primary w-50 w-50 bg-success text-white p-2" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Register</button>
+            <div className="modal-body px-4">
+              <input type="email" placeHolder='Email*' className='w-100 p-2' />
+              <input type="password" placeHolder='Password*' className='w-100 p-2 mt-4' />
+              <div className="dflex justify-content-between">
+                <div className="input-group my-3 d-flex justify-content-between align-items-center">
+                  <div className="input-group-text ">
+                    <input className="form-check-input mt-0 me-2 " type="checkbox" value="" aria-label="Checkbox for following text input" />
+                    <p className='mt-3'>Remember me?</p>
+                  </div>
+                  <p className='mt-2'>Forgot Password?</p>
+                </div>
+              </div>
+            </div>
+            <div className="modal-footer p-3 text-center">
+              <button className='btn bg-success text-white  w-100 p-2' >LOGIN</button>
+              <p className='w-100'>or Connect with Social Networks</p>
+              <div className='w-100'><i className="bi bi-facebook"></i></div>
+              <p className='w-100'>By creating an account you are accepting our Terms & Conditions</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabIndex="-1">
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button className="btn text-white bg-dark w-50 p-2" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Login</button>
+              <button className="btn btn-primary w-50 w-50 bg-success text-white p-2" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Register</button>
 
-                        </div>
-                        <div className="modal-body px-5">
-                            <input type="text" placeHolder='Username*' className='w-100 p-2 my-3' />
-                            <input type="email" placeHolder='Email*' className='w-100 p-2' />
-                            <input type="password" placeHolder='Password*' className='w-100 p-2 my-3' />
-                            <input type="password" placeHolder='Re-enter Password*' className='w-100 p-2 ' />
-                        </div>
-                        <div className="modal-footer">
-                            <p class='p-2'>Your personal data will be used to support your experience throughout this website, to manage access to your account, and for other purposes described in our <a href="" className='text-info-emphasis'>privacy policy.</a> </p>
-                            <div className="options1 w-100 px-3">
-                                <select className='m-3 p-2'>
-                                    <option value="All Locations">Agency</option>
-                                    <option value="Paris">Owner/Buyer</option>
-                                    <option value="Amsterdam">Agent</option>
-                                </select>
-                            </div>
-                        <button className="btn bg-success text-white w-100 p-2 mb-3">Login</button>    
-                        </div>
-                    </div>
-                </div>
             </div>
+            <div className="modal-body px-5">
+              <input type="text" placeHolder='Username*' className='w-100 p-2 my-3' />
+              <input type="email" placeHolder='Email*' className='w-100 p-2' />
+              <input type="password" placeHolder='Password*' className='w-100 p-2 my-3' />
+              <input type="password" placeHolder='Re-enter Password*' className='w-100 p-2 ' />
+            </div>
+            <div className="modal-footer">
+              <p class='p-2'>Your personal data will be used to support your experience throughout this website, to manage access to your account, and for other purposes described in our <a href="" className='text-info-emphasis'>privacy policy.</a> </p>
+              <div className="options1 w-100 px-3">
+                <select className='m-3 p-2'>
+                  <option value="All Locations">Agency</option>
+                  <option value="Paris">Owner/Buyer</option>
+                  <option value="Amsterdam">Agent</option>
+                </select>
+              </div>
+              <button className="btn bg-success text-white w-100 p-2 mb-3">Login</button>
+            </div>
+          </div>
+        </div>
+      </div>
       
     </>
   )
